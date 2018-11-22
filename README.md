77f8ad22db175dd66e283b847362eb9354417799
ecool50 <ewillie@sfu.ca>
2018-11-21 20:14:55 -0800

    First homework commit

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# hw08-seed
+# Homework 08 Elijah Willie
 
-This repo contains the BC Liquor app as a boilerplate for students to add to in Homework 08.
+This repo contains my solutions to STAT 545 homework 08. For this assignment, I took a previously developed app and added some new functionalities.
+These functionalities are described below.
 
-The code and data are from [Dean Attali's tutorial](https://deanattali.com/blog/building-shiny-apps-tutorial). The code can specifically be found [here](https://deanattali.com/blog/building-shiny-apps-tutorial/#12-final-shiny-app-code).
+# New features added
+
+For this assigment, I added a few new features to the `BC liquor stores app`. Their descriptions can be found below.
+
+* The results table has been made interactive.
+* I have made separate tabs to display the plots and the tables.
+* I have included the option to sort by price for the tables.
+* I have included the option to pick a theme from a host of themes.
+* I have include the `BC liquor stores` logo at the bottom of the page.
+
+# Links of possible interests
+
+* [Assignment Criteria]()
+* [Deployed App]()
+* [Source Code]()
+
+# Caveats
+* Some of the work here may have been borrowed and modified from other sources.
+* This was purely done to illustrate some ideas
+* I take no credit for parts of the work that are not mine
+* Let credit be given where it is due. Lets give to Ceasar what belongs to Ceasar
+* The code and data are from [Dean Attali's tutorial](https://deanattali.com/blog/building-shiny-apps-tutorial). The code can specifically be found [here](https://deanattali.com/blog/building-shiny-apps-tutorial/#12-final-shiny-app-code).
 
 
--- bcl/app.R
+++ bcl/app.R
@@ -1,27 +1,52 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(shinythemes)
+library(DT)
+library(rsconnect)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
+  
+ 
+  
+  #add a theme selector so the user can decide what theme to view the app in
+  themeSelector(),
+  
   titlePanel("BC Liquor Store prices"),
   sidebarLayout(
+    
     sidebarPanel(
+      
+      
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      
+      
       radioButtons("typeInput", "Product type",
                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                   selected = "WINE"),
-      uiOutput("countryOutput")
+      
+      uiOutput("countryOutput"),
+      
+      checkboxInput("sort", "Sort by Price", value = FALSE)
     ),
+    
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      #create the tab panels
+      tabsetPanel(type = "tabs",
+                 
+                  tabPanel("Plot", plotOutput("coolplot")),
+                  tabPanel("Results", dataTableOutput("results"))
+                  )
+      
     )
-  )
+  ),
+  
+  img(src = "BC_Liquor_Stores.png")
 )
 
+#setup the server function
 server <- function(input, output) {
   output$countryOutput <- renderUI({
     selectInput("countryInput", "Country",
@@ -34,12 +59,29 @@ server <- function(input, output) {
       return(NULL)
     }    
     
+    #if we are sorting by price
+    if(input$sort){
+      
     bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
-      )
+      ) %>% arrange(Price)
+    }
+    
+    #if we are not
+    else{
+        
+      bcl %>%
+        filter(Price >= input$priceInput[1],
+               Price <= input$priceInput[2],
+               Type == input$typeInput,
+               Country == input$countryInput
+        )
+      
+      
+    }
   })
   
   output$coolplot <- renderPlot({
@@ -50,7 +92,7 @@ server <- function(input, output) {
       geom_histogram()
   })
 
-  output$results <- renderTable({
+  output$results <- renderDataTable({
     filtered()
   })
 }
